f134ea320bbe0e7aba53053f8934c170cec3b4b3
christopher <stopher.kirkum@gmail.com>
2021-08-08 20:36:08 -0700

    small css changes

--- react-app/src/components/auth/LogoutButton.js
+++ react-app/src/components/auth/LogoutButton.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import { Redirect, useHistory } from 'react-router-dom';
 import { logout } from '../../store/session';
 import "./logoutButton.css"
 
+
 const LogoutButton = () => {
+  const history = useHistory();
   const dispatch = useDispatch()
   const onLogout = async (e) => {
     await dispatch(logout());
+    history.push('/' );
   };
 
   return (
